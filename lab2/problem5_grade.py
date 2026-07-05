c500fa05c575bd724937e6cb641f6acfad3ef501
rubiattahsin2004 <rubiat.tahsin@gmail.com>
2026-07-05 23:32:26 +0600

    Create problem5_grade.py

---
+++ lab2/problem5_grade.py
@@ -0,0 +1,20 @@
+marks = float(input("Enter marks: "))
+
+if marks >= 90:
+    print("Grade: A+")
+elif marks >= 85:
+    print("Grade: A")
+elif marks >= 80:
+    print("Grade: A-")
+elif marks >= 75:
+    print("Grade: B+")
+elif marks >= 70:
+    print("Grade: B")
+elif marks >= 65:
+    print("Grade: B-")
+elif marks >= 60:
+    print("Grade: C+")
+elif marks >= 50:
+    print("Grade: C")
+else:
+    print("Grade: F")
