73fa651373a12c75598fcaa94879bb809e99af10
rubiattahsin2004 <rubiat.tahsin@gmail.com>
2026-07-05 23:30:19 +0600

    Create problem1_swap.py

---
+++ lab2/problem1_swap.py
@@ -0,0 +1,12 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("\nBefore Swapping:")
+print("a =", a)
+print("b =", b)
+
+a, b = b, a
+
+print("\nAfter Swapping:")
+print("a =", a)
+print("b =", b)
