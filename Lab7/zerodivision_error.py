040cd9401cd5fc4f04ca278c7f76e4370af5e417
rubiattahsin2004 <rubiat.tahsin@gmail.com>
2026-09-09 00:00:02 +0600

    Create zerodivision_error.py

---
+++ Lab7/zerodivision_error.py
@@ -0,0 +1,10 @@
+try:
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+
+    result = num1 / num2
+
+    print("Result:", result)
+
+except ZeroDivisionError:
+    print("Error: Cannot divide a number by zero.")
